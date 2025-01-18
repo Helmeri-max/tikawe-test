2243dda0d430d41eaf52f6fb3c51c3a0a0c7c76f
Samuli Mikola <samulimikola@gmail.com>
2025-01-19 01:35:23 +0200

    Update app

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Heipparallaa!"
+
+@app.route("/page1")
+def page1():
+	return "Eka sivu"
+
+@app.route("/page2")
+def page2():
+	return "Toka sivu"
